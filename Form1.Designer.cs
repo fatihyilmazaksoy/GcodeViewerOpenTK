8fd0449c763419b40e36387fdd070f05271863e7
fatihyilmazaksoy <fatihyilmazaksoy@outlook.com>
2025-06-05 23:33:47 +0300

    ilk yükleme

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -26,7 +26,6 @@
         {
             BtnLoadFile = new Button();
             glControl1 = new OpenTK.GLControl.GLControl();
-            glControl2 = new OpenTK.GLControl.GLControl();
             openFileDialog1 = new OpenFileDialog();
             SuspendLayout();
             // 
@@ -46,26 +45,14 @@
             glControl1.APIVersion = new Version(3, 3, 0, 0);
             glControl1.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
             glControl1.IsEventDriven = true;
-            glControl1.Location = new Point(12, 65);
+            glControl1.Location = new Point(12, 71);
             glControl1.Name = "glControl1";
             glControl1.Profile = OpenTK.Windowing.Common.ContextProfile.Core;
             glControl1.SharedContext = null;
             glControl1.Size = new Size(467, 316);
             glControl1.TabIndex = 2;
             // 
-            // glControl2
-            // 
-            glControl2.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
-            glControl2.APIVersion = new Version(3, 3, 0, 0);
-            glControl2.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
-            glControl2.IsEventDriven = true;
-            glControl2.Location = new Point(552, 438);
-            glControl2.Name = "glControl2";
-            glControl2.Profile = OpenTK.Windowing.Common.ContextProfile.Core;
-            glControl2.SharedContext = null;
-            glControl2.Size = new Size(75, 23);
-            glControl2.TabIndex = 3;
-            // 
+        
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
@@ -75,7 +62,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(534, 411);
-            Controls.Add(glControl2);
+            Controls.Add(glControl1);
             Controls.Add(glControl1);
             Controls.Add(BtnLoadFile);
             Name = "Form1";
@@ -88,7 +75,6 @@
 
         private System.Windows.Forms.Button BtnLoadFile;
         private OpenTK.GLControl.GLControl glControl1;
-        private OpenTK.GLControl.GLControl glControl2;
         private OpenFileDialog openFileDialog1;
     }
 }
